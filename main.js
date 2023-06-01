8e8ce33a81aefa153b9849ec8fa07c6d6feb0f16
Ariel Rubilar <ariel.rubilar2@gmail.com>
2023-06-01 17:43:57 -0400

    [init]: soluction challenger

---
+++ main.js
@@ -0,0 +1,59 @@
+const required = () =>  ( value ) => {
+    return value && value.length > 0
+}
+
+const pattern = ( regex ) => ( value ) => {
+    return regex.test(value)
+}
+
+const regexEmail = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+const d = document
+
+const handleSubmit =( form , cardForm , cardMessage ) =>   ( event ) => {
+    event.preventDefault()
+
+    const emailInput =  form.querySelector('[name="email"]')
+    const email = emailInput.value
+    
+    const validations = [
+        required(),
+        pattern(regexEmail)
+    ]
+    
+    const isValidEmail = validations.reduce( (prev ,validation) => {
+       if( !validation(email) ) return false
+       return prev
+    }, true)
+
+    emailInput.toggleAttribute('aria-invalid', !isValidEmail)
+
+    if(isValidEmail) {
+
+        const spanEmail = cardMessage.querySelector('#span-email')
+        spanEmail.innerText = email
+        cardForm.classList.toggle('d-none')
+        cardMessage.classList.toggle('d-none')
+        form.reset();
+    }
+}
+
+const handleClickDismiss = (cardForm , cardMessage) => () => {
+    cardForm.classList.toggle('d-none')
+    cardMessage.classList.toggle('d-none')
+}
+
+
+d.addEventListener("DOMContentLoaded", (event) =>  {
+    const $document = event.target
+    const $form = $document.querySelector('#form')
+
+    const $cardForm = $document.querySelector('#card-form')
+    const $cardMessage = $document.querySelector('#card-message')
+
+    const $btnDismiss =  $document.querySelector('#bnt-dismiss-message')
+
+    $form.addEventListener('submit', handleSubmit($form , $cardForm , $cardMessage))
+
+    $btnDismiss.addEventListener( 'click', handleClickDismiss($cardForm, $cardMessage) )
+});
+
